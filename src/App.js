4fa8add347e5325a15481fd7b581956324f5c9a8
grace-barar <gbarar@gmail.com>
2021-01-25 12:09:42 -0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+
+import './App.css';
+function App() {
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      GB CHAT
+    </header>
+  </div>
+}
+export default App;
